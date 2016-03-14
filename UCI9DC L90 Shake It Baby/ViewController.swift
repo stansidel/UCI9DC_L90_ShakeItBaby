31cd2b610a8f852971232990688d12da240645c3
Stanislav Sidelnikov <sidelnikov.stanislav@gmail.com>
2016-03-14 12:11:15 +0500

    Random sounds on shake

--- UCI9DC L90 Shake It Baby/ViewController.swift
+++ UCI9DC L90 Shake It Baby/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    private var soundFilesNames: [NSURL]?
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        soundFilesNames = NSBundle.mainBundle().URLsForResourcesWithExtension("mp3", subdirectory: "Sounds")
+        if soundFilesNames == nil || soundFilesNames?.count == 0 {
+            NSLog("Cannot find sound files in directory specified")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        guard let soundFilesNames = soundFilesNames where soundFilesNames.count > 0 else {
+            return
+        }
+        if event?.subtype == .MotionShake {
+            player?.stop()
+            let i = Int(arc4random_uniform(UInt32(soundFilesNames.count)))
+            do {
+                player = try AVAudioPlayer(contentsOfURL: soundFilesNames[i])
+            } catch let error {
+                NSLog("Unable to create player for file \(soundFilesNames[i]). Error: \(error)")
+                return
+            }
+            print("Playing sound \(player!.url!.relativeString!)")
+            player?.play()
+        }
+    }
 
 }
 
